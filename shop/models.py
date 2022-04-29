e3a97a8047f4fccf4c69ac044a13fdf17a33419d
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-29 02:09:31 +0200

    register models in admin

--- shop/models.py
+++ shop/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ('name')
+        ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'catregories'
 
@@ -15,20 +15,20 @@ class Category(models.Model):
         return self.name
 
 #Product Model
-class Product:
+class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(max_length=200, blank=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    avialable = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True) #adding time
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('name')
-        index_together = (('id', 'slug'))
-
-    def __str__(self):
-        return self.name
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        
+        def __str__(self):
+            return self.name
