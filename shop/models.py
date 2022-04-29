b41ef7da1435eb56fe58e9383e00fa8d4558c7af
Mazen Ramadan <mazenramadan00@gmail.com>
2022-04-29 06:38:38 +0200

    base, list&detail html views

--- shop/models.py
+++ shop/models.py
@@ -36,5 +36,5 @@ class Product(models.Model):
         def __str__(self):
             return self.name
         
-        def get_absolute_url(self):
-            return reverse('shop:product_details', args=[self.id, self.slug])
+    def get_absolute_url(self):
+        return reverse('shop:product_details', args=[self.id, self.slug])
